3d77c47558a42f0bff3886bdfe0e9ee9f6b1e521
Matheus Silverio <matheusphillipo@gmail.com>
2024-06-25 00:09:48 -0300

    add: CORS policy and environment variables

--- src/server.js
+++ src/server.js
@@ -1,17 +1,28 @@
 import http from "node:http";
 import { URL } from "node:url";
 import { getPokemonData } from "./service/pokemon_service.js";
+import dotenv from "dotenv";
 
-const host = "localhost";
-const port = 8000;
+dotenv.config();
 
 const requestListener = async function (req, res) {
   const parsedUrl = new URL(req.url, `http://${req.headers.host}`);
   const pokemonName = parsedUrl.pathname.substring(1); //
 
+  // Adicionar cabeçalhos CORS
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    res.writeHead(204);
+    res.end();
+    return;
+  }
+
   try {
     const data = await getPokemonData(pokemonName);
-
+    res.writeHead(200, { "Content-Type": "application/json" });
     res.end(JSON.stringify(data));
   } catch (error) {
     res.writeHead(500, { "Content-Type": "application/json" });
@@ -20,6 +31,8 @@ const requestListener = async function (req, res) {
 };
 
 const server = http.createServer(requestListener);
-server.listen(port, host, () => {
-  console.log(`Server is running on http://${host}:${port}`);
+server.listen(process.env.PORT, process.env.HOST, () => {
+  console.log(
+    `Server is running on http://${process.env.HOST}:${process.env.PORT}`
+  );
 });
