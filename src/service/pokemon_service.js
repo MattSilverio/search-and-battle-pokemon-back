93ecca056a6f5ea28ba3a8d3bef90f9a78d31a31
Matheus Silverio <matheusphillipo@gmail.com>
2024-06-25 00:11:27 -0300

    chore(Pokemon): Treating error responses

--- src/service/pokemon_service.js
+++ src/service/pokemon_service.js
@@ -1,16 +1,23 @@
-import axios from "axios";
-const BASE_URL = "https://pokeapi.co/api/v2/pokemon";
+import axios, { HttpStatusCode } from "axios";
 
 export const getPokemonData = async (pokemonName) => {
   if (!pokemonName) {
     throw new Error("Pokemon Not Specified");
   }
 
+  const pokemonNameToLowerCase = pokemonName.toLowerCase();
+
   try {
-    const response = await axios.get(`${BASE_URL}/${pokemonName}`);
+    const response = await axios.get(
+      `${process.env.BASE_URL}/${pokemonNameToLowerCase}`
+    );
 
     return response.data;
   } catch (error) {
+    if (error.response.status === HttpStatusCode.NotFound) {
+      throw new Error("Pokemon Not Found");
+    }
+
     throw new Error(error);
   }
 };
